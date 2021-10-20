0fd97b9ba83b333532dabe964e63d27e2521e063
朴海淋 <piaohailin@163.com>
2021-10-20 23:58:52 +0800

    本地编译

--- native.sh
+++ native.sh
@@ -3,6 +3,17 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-21.2.0\ 2/C
 export export PATH=$PATH:$JAVA_HOME/bin:.
 java -version
 
-mvn package
-mvn exec:exec
+#mvn package
+
+#运行预热java代码比二进制代码效率提高30毫秒
+#java -cp ./target/classes com.mycompany.app.App
+
+jar cfvm App.jar manifest.txt -C target/classes .
+jar tf App.jar
+java -jar App.jar
+
+#运行本地编译程序启动无感
+native-image --language:js -jar App.jar
+
+#mvn exec:exec
 #mvn exec:exec@nograal
